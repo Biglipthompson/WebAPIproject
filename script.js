f6d581534a10296b9efed0afd3b63adcd8257abd
Ja'Quan Thompson <jaquanthompson87@icloud.com>
2020-12-17 23:31:38 -0500

    commit
    working on the JS.

---
+++ script.js
@@ -0,0 +1,28 @@
+let employees = [];
+const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
+email, location, phone, dob &noinfo &nat=US`
+const gridContainer = document.querySelector('.grid-container');
+const overlay = document.querySelector('.overlay');
+const modalContainer = document.querySelector('.modal-text');
+const modalClose = document.querySelector('.modal-close');
+
+fetch(urlAPI)
+.then(res => res.json())
+.then(res => res.results)
+.then(displayEmployees)
+.catch(err => console.log(err))
+
+
+function displayEmployees(employeeData) {
+    
+    employees = employeeData;
+
+    let employeeHTML = '';
+
+    employee.forEach((employee, index) => {
+        let name = employee.name;
+        let email = employee.email;
+        let city = employee.location.city;
+        let picture = employee.picture;
+    }
+}
